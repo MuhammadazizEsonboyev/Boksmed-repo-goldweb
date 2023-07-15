4b8899b4c59e645902a98ee15bc7251e9511f2f9
faer17 <faerumidovna17@gmail.com>
2023-07-15 10:33:38 +0500

    fix

---
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from "../../assets/image/logo.svg"
+
+
+export default function Navbar() {
+  return (
+    <div>
+     <nav>
+        <img src={logo} alt="" className="logo" />
+     </nav>
+    </div>
+  )
+}
