5c904ee5d8f85e1ea9930c141ff23dbecb5594f2
faer17 <faerumidovna17@gmail.com>
2023-07-15 11:50:26 +0500

    fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import img from "./assets/image/headerpic.svg"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <>
-      <img src={img} alt="" />
+      <FontAwesomeIcon icon={faEnvelope} />
     </>
   )
 }
