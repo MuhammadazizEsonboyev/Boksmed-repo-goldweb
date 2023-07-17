19a4ed577a4a4b056805ada84a965a6f1eb89764
bobur2006coder <boburdilmurodov702@gmail.com>
2023-07-17 14:06:24 +0500

    fix

--- vite-project/src/assets/index.js
+++ vite-project/src/assets/index.js
@@ -3,3 +3,9 @@ import card2 from "../../src/assets/image/Sendimage2.svg"
 import card3 from "../../src/assets/image/Sendimage3.svg"
 
 export { card1, card2, card3 }
+
+import twoImgNav from "../assets/image/telegram 3.svg"
+import oneImgNav from "../assets/image/viber 3.svg"
+import threeImgNav from "../assets/image/whatsapp 3.svg"
+
+export { oneImgNav, threeImgNav, twoImgNav }
--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -1,9 +1,15 @@
 import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Section from "../Section/Section";
 import SectionEnd from "../SectionEnd/SectionEnd";
-
+import MapPage from "../YandexMap/MapPage";
+import 'virtual:windi.css'
 export default function Home() {
   return (
     <>
+    <Header/>
+    <MapPage/>
+<Section/>
       <SectionEnd />
       <Footer/> 
     </>
