0202fa390fae57d338313222cfd4af82177fa94f
Asadbek0905 <asadbekurozmatov965@gmail.com>
2023-07-17 14:37:30 +0500

    fix

--- vite-project/src/components/Home/Home.jsx
+++ vite-project/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
